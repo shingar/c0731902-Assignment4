ce81c6fe9a942fc9e0f0103f29f6ce0f17a751d5
visualstudio <visualstudio@122-PC25>
2019-04-05 18:47:20 -0400

    commit1

--- c0731902-assignment4/Program.cs
+++ c0731902-assignment4/Program.cs
@@ -8,8 +8,46 @@ namespace c0731902_assignment4
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
         }
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+
+        public void ReadTextFiles()
+        {
+            //Read file using StreamReader.Reads file line by line
+            using (StreamReader file = new StreamReader("c:/area51/beowulf.txt"))
+            {
+                int counter = 0;
+                string ln;
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+
+                }
+                file.close();
+                Console.WriteLine($"File has{counter}lines");
+            }
+        }
+        public int FindNumberofBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+                if (char.IsWhiteSpace(c)) { countSpaces++; }
+            }
+            return countSpaces;
+        }
+
+
     }
 }
